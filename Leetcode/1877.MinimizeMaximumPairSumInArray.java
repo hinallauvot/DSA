80198907ab327edbbae9584d7255aa710a425820
Hinal Lauvot <hinallauvot@gmail.com>
2023-11-17 21:01:37 +0530

    Adding Java Solution

---
+++ Leetcode/1877.MinimizeMaximumPairSumInArray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minPairSum(int[] nums) 
+    {
+        Arrays.sort(nums);
+        int first = 0;
+        int second = nums.length-1;
+        int max = 0;
+        while(first < second)
+        {
+            max = Math.max(max, nums[first] + nums[second]);
+            first++;
+            second--;
+        }
+
+        return max;
+    }
+}
