48d2e4781ee7fdb436309b6bf62f14b219faff56
Hinal Lauvot <hinallauvot@gmail.com>
2024-01-05 22:33:03 +0530

    Adding Java Solution

--- Leetcode/300.LongestIncreasingSubsequence.java
+++ Leetcode/300.LongestIncreasingSubsequence.java
@@ -15,3 +15,19 @@ class Solution {
         return solve(nums, 0, -1, dp);
     }
 }
+//Approach 2 using binary search
+public class Solution {
+    public int lengthOfLIS(int[] nums) {            
+        int[] dp = new int[nums.length];
+        int len = 0;
+
+        for(int x : nums) {
+            int i = Arrays.binarySearch(dp, 0, len, x);
+            if(i < 0) i = -(i + 1);
+            dp[i] = x;
+            if(i == len) len++;
+        }
+
+        return len;
+    }
+}
